a2b51f5fd16835f62e0b8b525366c0fc662c6d90
Carlos Marx <carlosmarx27@gmail.com>
2020-03-25 23:51:19 -0300

    Create logon feature and layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import Header from './Header'
+import './global.css';
+
+import Routes from './routes';
+
 
 function App() {
+
   return (
-    <Header>
-      Be the Hero!
-    </Header>
+      <Routes />
+      
   );
 }
 
