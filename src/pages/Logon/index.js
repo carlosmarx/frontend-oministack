72cc61f3e5ef6d6341c744bc1805fd479a02d4a7
Carlos Marx <carlosmarx27@gmail.com>
2020-03-26 12:56:57 -0300

    Template register

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import {FiLogIn} from 'react-icons/fi';
 
 import './style.css';
@@ -10,7 +11,7 @@ export default function Logon() {
   return (
       <div className="logon-container">
         <section className="form">
-          <img src={logoImg} alt="Be the Hero!   "/>
+          <img src={logoImg} alt="Be the Hero! "/>
 
           <form>
             <h1>Faça seu logon</h1>
@@ -19,10 +20,10 @@ export default function Logon() {
 
             <button className="button" type="submit">Entrar</button>
 
-            <a href="/register">
+            <Link className="back-link" to="/register">
               <FiLogIn size={16} color={'#e02041'}/>
               Não tenho cadastro! 
-            </a>
+            </Link>
           </form>
         </section>
 
